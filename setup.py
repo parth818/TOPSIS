4a6d1d0252b2d761016aa1bdbf080d669dc8ab6a
PARTH VERMA <vermaparth818@gmail.com>
2020-11-10 00:16:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A simplified package to perform TOPSIS Analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/parth818/TOPSIS-Parth-101983047",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
